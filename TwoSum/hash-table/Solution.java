a9fb0410041fc82dc63ba9cd4c86d45f1969440f
YoLiMow <frank29364133@gmail.com>
2024-06-10 16:45:27 +0800

    init

--- TwoSum/hash-table/Solution.java
+++ TwoSum/hash-table/Solution.java
@@ -1,8 +1,8 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class twoSum {
-    public int[] twosum(int[] nums, int target) {
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int current = nums[i];

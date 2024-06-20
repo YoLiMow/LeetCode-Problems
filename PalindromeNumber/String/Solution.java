86c9a501f62ff20b7d7d29f8173b59285432e05d
YoLiMow <frank29364133@gmail.com>
2024-06-20 22:06:49 +0800

    init

---
+++ PalindromeNumber/String/Solution.java
@@ -0,0 +1,9 @@
+package PalindromeNumber.String;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String str = Integer.toString(x);
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+}
